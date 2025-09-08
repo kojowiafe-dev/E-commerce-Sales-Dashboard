441d16f59443001135bf6089c08c08698fc5302e
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-08 10:05:21 +0000

    added files

--- server/app/main.py
+++ server/app/main.py
@@ -20,7 +20,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
-    # allow_origins=["http://10.21.16.62:5173/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
