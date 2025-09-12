42a1d9c3f091088f52bc9ca5e3b9a287889ce9ba
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-12 04:32:45 +0000

    commit

--- server/app/database.py
+++ server/app/database.py
@@ -11,12 +11,14 @@ import os
 load_dotenv()
 
 
-sqlite_file_name='sales.db'
-SQLALCHEMY_DATABASE_URL=f"sqlite:///./{sqlite_file_name}"
+# sqlite_file_name='sales.db'
+# SQLALCHEMY_DATABASE_URL=f"sqlite:///./{sqlite_file_name}"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://salesuser:Secure123@localhost:5432/salesdb"
+
 # DATABASE_URL = os.getenv("DATABASE_URL")
 # setup the engine for the database
-engine=create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
-# engine=create_async_engine(DATABASE_URL, echo=False, future=True, pool_size=20, max_overflow=40, pool_timeout=60)
+# engine=create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+engine=create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False, future=True, pool_size=20, max_overflow=40, pool_timeout=60)
 
 async def get_session():
     async with AsyncSession(engine) as session:
@@ -38,8 +40,8 @@ async def create_db_and_tables():
     # SQLModel.metadata.drop_all(engine)
 
 
-@event.listens_for(Engine, "connect")
-async def enable_sqlite_foreign_keys(dpapi_connection, connection_record):
-    cursor = await dpapi_connection.cursor()
-    await cursor.execute("PRAGMA foreign_keys=ON")
-    await cursor.close()
+# @event.listens_for(Engine, "connect")
+# async def enable_sqlite_foreign_keys(dpapi_connection, connection_record):
+#     cursor = await dpapi_connection.cursor()
+#     await cursor.execute("PRAGMA foreign_keys=ON")
+#     await cursor.close()
