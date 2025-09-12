3e59f67d598323340ec7f13817c1f60edcb7d65b
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-12 03:55:36 +0000

    altering the endpoints

---
+++ server/app/routers/products.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import select, Session
+from sqlalchemy.ext.asyncio import AsyncSession
+import asyncio
+from schemas import ProductCreate, ProductResponse
+from database import SessionDep
+from typing import List
+from models.model import Product
+
+router = APIRouter()
+
+@router.post("/", response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductCreate, db: AsyncSession = Depends(SessionDep)):
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    await db.commit()
+    await db.refresh(db_product)
+    return db_product
+
+@router.get("/", response_model=List[ProductResponse])
+async def read_products(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(SessionDep)):
+    products = await db.exec(select(Product).offset(skip).limit(limit)).all()
+    return products
