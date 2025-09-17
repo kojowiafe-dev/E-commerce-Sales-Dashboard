1dbafe5b8088c7d5e795ab699a483a3760ce3684
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-17 18:52:23 +0000

    using clean architecture

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -1,98 +1,67 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime, date
-from typing import Optional, List
-from uuid import uuid4
-from pydantic import EmailStr
-from models.model import RoleEnum
+# from sqlmodel import SQLModel, Field
+# from datetime import datetime, date
+# from typing import List
+# from uuid import uuid4
+# from pydantic import EmailStr
+
+# # -----------------------
+# # Product Schemas
+# # -----------------------
+
 
     
     
-class UserBase(SQLModel):
-    name: str
-    email: Optional[str] = None
-    phone: str
-    house_address: str
-    role: RoleEnum
-
-class UserRegister(UserBase):
-    password: str
-
-class UserLogin(SQLModel):
-    name: str
-    password: str
-    role: str
     
+# # -----------------------
+# # OrderItem Schemas
+# # -----------------------
     
+# class OrderItemBase(SQLModel):
+#     quantity: int
+#     price_each: float
+#     line_total: float
 
-# -----------------------
-# Product Schemas
-# -----------------------
+# class OrderItemCreate(OrderItemBase):
+#     product_id: int
 
-class ProductBase(SQLModel):
-    name: str
-    price_each: float
+# class OrderItemResponse(OrderItemBase):
+#     order_item_id: int
+#     product: ProductResponse   # nested product info
 
-class ProductCreate(ProductBase):
-    pass
-
-class ProductResponse(ProductBase):
-    product_id: int
-
-    class Config:
-        orm_mode = True
-    
-    
-    
-# -----------------------
-# OrderItem Schemas
-# -----------------------
-    
-class OrderItemBase(SQLModel):
-    quantity: int
-    price_each: float
-    line_total: float
-
-class OrderItemCreate(OrderItemBase):
-    product_id: int
-
-class OrderItemResponse(OrderItemBase):
-    order_item_id: int
-    product: ProductResponse   # nested product info
-
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
         
         
        
        
-# -----------------------
-# Order Schemas
-# ----------------------- 
+# # -----------------------
+# # Order Schemas
+# # ----------------------- 
 
-class OrderBase(SQLModel):
-    order_date: datetime
-    purchase_address: str
+# class OrderBase(SQLModel):
+#     order_date: datetime
+#     purchase_address: str
 
-class OrderCreate(OrderBase):
-    items: List[OrderItemCreate]   # create order with items
+# class OrderCreate(OrderBase):
+#     items: List[OrderItemCreate]   # create order with items
 
-class OrderResponse(OrderBase):
-    order_id: int
-    items: List[OrderItemResponse]
+# class OrderResponse(OrderBase):
+#     order_id: int
+#     items: List[OrderItemResponse]
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
         
         
         
-class TokenData(SQLModel):
-    username: Optional[str] = None
+# # class TokenData(SQLModel):
+# #     username: Optional[str] = None
 
 
-class Token(SQLModel):
-    access_token: str
-    token_type: str
-    role: str
+# # class Token(SQLModel):
+# #     access_token: str
+# #     token_type: str
+# #     role: str
 
-class ForgotPasswordRequest(SQLModel):
-    email: EmailStr
+# class ForgotPasswordRequest(SQLModel):
+#     email: EmailStr
