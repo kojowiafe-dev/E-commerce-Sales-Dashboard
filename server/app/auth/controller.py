6a0bbf149a3c56326e1e11ee6caca8f6863e82e3
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-17 02:56:00 +0000

    commit

--- server/app/auth/controller.py
+++ server/app/auth/controller.py
@@ -16,15 +16,16 @@ router = APIRouter(
 @limiter.limit("5/hour")
 async def register_user(request: Request, session: SessionDep,
                       register_user_request: schemas.UserRegister):
-    await service.register(register_user_request, session)
-    
-    
-    
+    registered_user = await service.register(register_user_request, session)
+    return registered_user
+
+
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Token)
 @limiter.limit("5/hour")
 async def login_user(request: Request, session: SessionDep,
                       login_user_request: schemas.UserLogin):
-    await service.login(login_user_request, session)
+    login_response = await service.login(login_user_request, session)
+    return login_response
 
 
 @router.post("/token", response_model=schemas.Token)
