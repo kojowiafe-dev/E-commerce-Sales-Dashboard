04b4fbaeda9f206789b3c792941d7f52fa4ca768
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-18 04:11:10 +0000

    commit

--- server/app/api/api.py
+++ server/app/api/api.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 from routers.products.controller import router as products_router
 from routers.users.controller import router as users_router
 from routers.auth.controller import router as auth_router
+from routers.orders.controller import router as orders_router
+from routers.orderitems.controller import router as orderitems_router
 
 
 def register_routes(app: FastAPI):
+    app.include_router(orders_router)
+    app.include_router(orderitems_router)
     app.include_router(products_router)
     app.include_router(auth_router)
     app.include_router(users_router)
