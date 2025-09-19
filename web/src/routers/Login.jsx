1ac9dfb3f71ca4c7dba61b3ae7f9d3ad7eafa65d
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-19 17:30:39 +0000

    feat: web altering

---
+++ web/src/routers/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="bg-black h-screen">
+      <div className="text-white">Left section</div>
+      <div className="text-white">Right section</div>
+    </div>
+  );
+};
+
+export default Login;
